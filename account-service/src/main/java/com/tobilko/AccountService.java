9cfd6d3dae8e1cd92de9202b1624d3e051fd3a0d
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-02-06 21:43:05 +0200

    made sm props more prioritised

--- account-service/src/main/java/com/tobilko/AccountService.java
+++ account-service/src/main/java/com/tobilko/AccountService.java
@@ -1,13 +1,7 @@
 package com.tobilko;
 
-import com.tobilko.entity.Account;
-import com.tobilko.entity.AccountCredentials;
-import com.tobilko.repository.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountService {
